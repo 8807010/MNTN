58d525a3407bd92618e49afdd3de39fba1127b3c
Artiom_Sokolov <sokolova-net@mail.ru>
2022-08-27 10:18:36 +0300

    mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,32 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+
+const navBtn = document.querySelector('.nav-button');
+const mobileMav = document.querySelector('.mobile-nav');
+const body = document.body;
+
+// Клик по кнопке
+navBtn.addEventListener('click', function (event) {
+  event.stopPropagation();
+  toggleMobileNav();
+});
+
+// Клик по окну за пределами навигации
+window.addEventListener('click', function () {
+  if (body.classList.contains('no-scroll')) {
+    toggleMobileNav();
+  }
+});
+
+// Останавливаем клик внутри открытой мобильной навигации
+mobileMav.addEventListener('click', function (event) {
+  event.stopPropagation();
+});
+
+function toggleMobileNav() {
+  mobileMav.classList.toggle('mobile-nav-active');
+  navBtn.classList.toggle('nav-button-close');
+  body.classList.toggle('no-scroll');
+};
