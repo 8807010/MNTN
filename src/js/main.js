ae05dc4da0cc73049c96b714caf226a28d4ea049
Artiom_Sokolov <sokolova-net@mail.ru>
2022-08-28 19:23:16 +0300

    parallax effect

--- src/js/main.js
+++ src/js/main.js
@@ -5,35 +5,5 @@
 
 // import './_mobileMenu';
 
-
-const navBtn = document.querySelector('.header__burger');
-const mobileNav = document.querySelector('.header__mobile');
-const body = document.body;
-
-console.log(mobileNav);
-
-
-// Клик по кнопке
-navBtn.addEventListener('click', function (event) {
-  event.stopPropagation();
-  toggleMobileNav();
-  toggleMobileNav.style.transition = "all 0.9s ease";
-});
-
-// Клик по окну за пределами навигации
-window.addEventListener('click', function () {
-  if (body.classList.contains('no-scroll')) {
-    toggleMobileNav();
-  }
-});
-
-// Останавливаем клик внутри открытой мобильной навигации
-mobileNav.addEventListener('click', function (event) {
-  event.stopPropagation();
-});
-
-function toggleMobileNav() {
-  mobileNav.classList.toggle('header__mobile-active');
-  navBtn.classList.toggle('header__mobile-close');
-  body.classList.toggle('no-scroll');
-};
+import './_bg';
+import './_burger';
